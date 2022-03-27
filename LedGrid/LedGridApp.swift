415b6070f979f0617036f89818623a4ae161515d
Ted <ted_bennett@icloud.com>
2022-03-27 18:35:37 +0100

    Initial Commit

---
+++ LedGrid/LedGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedGridApp.swift
+//  LedGrid
+//
+//  Created by Ted Bennett on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LedGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
