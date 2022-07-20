753efdf1c348ddb372eb92c4c47f93a941ccd166
tedbennett <ted_bennett@icloud.com>
2022-07-20 07:44:25 +0100

    Add device id upload

--- LedGrid/NetworkManager.swift
+++ LedGrid/NetworkManager.swift
@@ -13,7 +13,7 @@ class NetworkManager {
     private init() { }
     
     func postGrid(_ grid: ColorGrid, completion: @escaping (Error?) -> Void) {
-        let url = URL(string: "https://rlefhg7mpa.execute-api.us-east-1.amazonaws.com/upload")!
+        let url = URL(string: EnvironmentVariables.endpoint)!
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -31,4 +31,34 @@ class NetworkManager {
             return
         }
     }
+    
+    func postToken(_ token: String) {
+        let url = URL(string: "\(EnvironmentVariables.endpoint)/id")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let payload = TokenPayload(user: EnvironmentVariables.userId, device: token)
+        do {
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .iso8601
+            let data = try encoder.encode(payload)
+            request.httpBody = data
+            URLSession.shared.dataTask(with: request) { _, _, error in
+                print(error)
+            }.resume()
+        } catch {
+            return
+        }
+    }
 }
+
+struct GridPayload: Codable {
+    var grid: ColorGrid
+    var user: String
+}
+
+struct TokenPayload: Codable {
+    var user: String
+    var device: String
+}
+
