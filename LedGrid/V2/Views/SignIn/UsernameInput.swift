74337fd5c2a9b96eb77f3a5c9f21313170ac2da8
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2024-03-15 12:07:24 +0000

    add username input when signing in for the first time

---
+++ LedGrid/V2/Views/SignIn/UsernameInput.swift
@@ -0,0 +1,18 @@
+//
+//  UsernameInput.swift
+//  LedGrid
+//
+//  Created by Ted Bennett on 22/02/2024.
+//
+
+import SwiftUI
+
+struct UsernameInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsernameInput()
+}
