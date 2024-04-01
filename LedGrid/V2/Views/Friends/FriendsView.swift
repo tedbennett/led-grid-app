90c1d806f46681190d01a3192f2077b70c02c626
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2024-04-01 17:06:31 -0400

    hide friends sections if none or too many

--- LedGrid/V2/Views/Friends/FriendsView.swift
+++ LedGrid/V2/Views/Friends/FriendsView.swift
@@ -12,31 +12,32 @@ struct FriendsView: View {
     var user: APIUser
     @Query var friends: [Friend] = []
     @Query var requests: [FriendRequest] = []
+    @State var showAllFriends = false
+    @State var showAllSent = false
+    @State var showAllReceived = false
 
+    var sent: [FriendRequest] {
+        requests.filter { $0.sent && $0.status == .sent }
+    }
+
+    var received: [FriendRequest] {
+        requests.filter { !$0.sent && $0.status == .sent }
+    }
 
     var body: some View {
         List {
+//            if !received.isEmpty {
+//                CardList(items: received, title: "Received Friend Requests")
+//            }
+
             NavigationLink("Find Friends") {
                 FriendSearchView()
             }
 
-            Section("Friends") {
-                ForEach(friends) { friend in
-                    VStack {
-                        UserCard(name: friend.name, username: friend.username) {}
-                    }
-                }
-            }
+            CardList(items: friends, title: "Friends")
 
-            Section("Sent Friend Requests") {
-                ForEach(requests.filter { $0.sent && $0.status == .sent }) { request in
-                    UserCard(name: request.name, username: request.username) {}
-                }
-            }
-            Section("Received Friend Requests") {
-                ForEach(requests.filter { !$0.sent && $0.status == .sent }) { request in
-                    UserCard(name: request.name, username: request.username) {}
-                }
+            if !sent.isEmpty {
+                CardList(items: sent, title: "Sent Friend Requests")
             }
         }
         .toolbar {
@@ -50,4 +51,44 @@ struct FriendsView: View {
 #Preview {
     FriendsView(user: APIUser.example)
         .modelContainer(Container.modelContainer)
+}
+
+protocol Card: Identifiable {
+    var id: String { get set }
+    var name: String? { get set }
+    var username: String { get set }
+}
+
+extension Friend: Card {}
+extension FriendRequest: Card {}
+
+struct CardList: View {
+    var items: [any Card]
+    var title: String
+    @State private var showAll = false
+
+    var body: some View {
+        if !items.isEmpty {
+            Section {
+                ForEach(items.prefix(showAll ? Int.max : 5), id: \.id) { item in
+                    UserCard(name: item.name, username: item.username) {}
+                }
+            } header: {
+                HStack {
+                    Text(title)
+                    Spacer()
+                    if items.count > 5 {
+                        Button {
+                            withAnimation {
+                                showAll.toggle()
+                            }
+                        } label: {
+                            Text(showAll ? "See Less" : "See More")
+                                .font(.caption)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
