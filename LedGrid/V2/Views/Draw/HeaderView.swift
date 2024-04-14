75ed1a7ab03fe14ca40374bb8d5465cc7dd89254
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2024-04-14 10:00:39 -0400

    add indicator for received friend requests

--- LedGrid/V2/Views/Draw/HeaderView.swift
+++ LedGrid/V2/Views/Draw/HeaderView.swift
@@ -5,10 +5,17 @@
 //  Created by Ted Bennett on 09/06/2023.
 //
 
+import SwiftData
 import SwiftUI
 
 struct HeaderView: View {
     @State private var path = NavigationPath()
+    @Query var requests: [FriendRequest] = []
+
+    var hasRequests: Bool {
+        !requests.filter { $0.status == .sent && !$0.sent }.isEmpty
+    }
+
     var body: some View {
         NavigationStack(path: $path) {
             HStack {
@@ -24,6 +31,13 @@ struct HeaderView: View {
                     Image(systemName: "person.2")
                 }
                 .buttonStyle(StdButton())
+                .overlay(
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .offset(x: 13, y: 13)
+                        .foregroundStyle(.red)
+                        .opacity(hasRequests ? 1 : 0)
+                )
                 Button {
                     if LocalStorage.user == nil {
                         NotificationCenter.default.post(name: Notification.Name.showSignIn, object: nil)
